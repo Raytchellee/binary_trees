465bb83edeb067ce332fb0bea14aa64a48003d95
raytchellee <amakaoforkaja@gmail.com>
2023-10-04 05:36:35 -0700

    task check

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,16 +8,10 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	binary_tree_t *current = NULL;
 	size_t depth = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
-	current = tree;
-	while (current->parent != NULL)
-	{
-		depth++;
-		current = current->parent;
-	}
-	return depth;
+	depth = 1 + binary_tree_depth(tree->parent);
+	return (depth);
 }
