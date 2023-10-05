4561a913880b67544b9a15217fe174c8bedb004a
raytchellee <amakaoforkaja@gmail.com>
2023-10-05 03:07:18 -0700

    task check

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the node
+ * Return: none
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int first, second;
+
+	if (tree == NULL)
+		return (0);
+	first = binary_tree_balance(tree);
+	second = binary_tree_is_full(tree);
+
+	return (first == 0 && second);
+}
