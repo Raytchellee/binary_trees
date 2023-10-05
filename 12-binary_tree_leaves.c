959755f95c07e7ae71bf03cd21e0f1146474d699
raytchellee <amakaoforkaja@gmail.com>
2023-10-05 01:57:19 -0700

    task check

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the node
+ * Return: none
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t first, second;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	first = binary_tree_leaves(tree->left);
+	second = binary_tree_leaves(tree->right);
+	
+	return (first + second);
+}
