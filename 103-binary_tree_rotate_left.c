efa9d160462bab9532a0c013be90e8a4eba30874
raytchellee <amakaoforkaja@gmail.com>
2023-10-05 16:27:38 -0700

    task check

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -12,20 +12,23 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 
 	if (!tree)
 		return (NULL);
+
 	c_node = p_node->right;
 	if (!c_node)
 		return (tree);
+
 	if (c_node->left)
-		c_node->left->p_node = p_node;
+		c_node->left->parent = p_node;
 
 	p_node->right = c_node->left;
 	c_node->left = p_node;
-	c_node->p_node = p_node->p_node;
-	p_node->p_node = c_node;
-	if (c_node->p_node && c_node->p_node->left == p_node)
-		c_node->p_node->left = c_node;
-	else if (c_node->p_node)
-		c_node->p_node->right = c_node;
+	c_node->parent = p_node->parent;
+	p_node->parent = c_node;
+
+	if (c_node->parent && c_node->parent->left == p_node)
+		c_node->parent->left = c_node;
+	else if (c_node->parent)
+		c_node->parent->right = c_node;
 
 	return (c_node);
 }
