cd46d4aad3174c779ca1ea7b1d30c21eef4fc758
raytchellee <amakaoforkaja@gmail.com>
2023-10-05 03:29:46 -0700

    task check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,26 +1,7 @@
 #include "binary_trees.h"
 #include "15-binary_tree_is_full.c"
-
-/**
- * get_height - measures the height of a binary tree
- * @tree: pointer to the root node
- * Return: none
- */
-
-size_t get_height(const binary_tree_t *tree)
-{
-	size_t first = 0, second = 0;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	first = get_height(tree->left);
-	second = get_height(tree->right);
-	if (first > second)
-		return (first + 1);
-	return (second + 1);
-}
+#include "14-binary_tree_balance.c"
+#include "11-binary_tree_size.c"
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -30,13 +11,12 @@ size_t get_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t first, second, third;
+	size_t first, second;
 
 	if (tree == NULL)
 		return (0);
-	first = get_height(tree->left);
-	second = get_height(tree->right);
-	third = binary_tree_is_full(tree);
+	first = binary_tree_size(tree->left);
+	second = binary_tree_size(tree->right);
 
-	return ((first == second) && third);
+	return (first == second);
 }
