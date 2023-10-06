7abc1cb92f83b9c272da379ce0c8acefca7c064b
raytchellee <amakaoforkaja@gmail.com>
2023-10-05 17:03:40 -0700

    task check

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -12,7 +12,7 @@ bst_t *bst_search(const bst_t *tree, int value)
 	if (!tree)
 		return (NULL);
 	if (tree->n == value)
-		return (tree);
+		return ((binary_tree_t *) tree);
 
 	if (value > tree->n)
 		return (bst_search(tree->right, value));
