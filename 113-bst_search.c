f184ecd21904d7d6b077dd39358a8def4839928c
raytchellee <amakaoforkaja@gmail.com>
2023-10-05 17:02:16 -0700

    task check

---
+++ 113-bst_search.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - search for value in bst
+ * @tree: root node
+ * @value: value to search
+ * Return: correct node or null
+ */
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+	if (tree->n == value)
+		return (tree);
+
+	if (value > tree->n)
+		return (bst_search(tree->right, value));
+
+	return (bst_search(tree->left, value));
+}
